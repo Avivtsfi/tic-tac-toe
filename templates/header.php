54123de6d9088a32e70219ee4e43038906bf798c
Avivtsfi <156768137+Avivtsfi@users.noreply.github.com>
2024-01-16 08:39:41 +0200

    Add files via upload

---
+++ templates/header.php
@@ -0,0 +1,21 @@
+<?php
+require_once "functions.php";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+   <!-- <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1">-->
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	 <link rel='stylesheet' href='style.css' type='text/css'/>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <title>TicTacToe game</title>
+
+   
+</head>
+<body>
+
+    <div class="wrapper">
